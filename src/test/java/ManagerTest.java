a82121fd78357db88bdafd80f65c25d31a82740b
Clau <clau.rjimenez@gmail.com>
2022-02-09 14:18:30 +0000

    Manager class and tests

---
+++ src/test/java/ManagerTest.java
@@ -0,0 +1,45 @@
+import management.Manager;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ManagerTest {
+    Manager manager;
+
+    @Before
+    public void before(){
+        manager = new Manager("John", "ABCD10", 100.00, "sales");
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("John", manager.getName());
+    }
+
+    @Test
+    public void hasSalary() {
+        assertEquals(100.00, manager.getSalary(), 0.0);
+    }
+
+    @Test
+    public void hasNI() {
+        assertEquals("ABCD10", manager.getNI());
+    }
+
+    @Test
+    public void hasDeptName(){
+        assertEquals("sales", manager.getDeptName());
+    }
+
+    @Test
+    public void canRaiseSalary() {
+        manager.raiseSalary(50.00);
+        assertEquals(150.00, manager.getSalary(), 0.0);
+    }
+
+    @Test
+    public void canPayBonus() {
+        assertEquals(1, manager.payBonus(), 0.0);
+    }
+}
