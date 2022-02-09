2140133d5d8efb3a116ae67dfb5ab439315a54d0
Clau <clau.rjimenez@gmail.com>
2022-02-09 14:47:49 +0000

    Director class and tests

---
+++ src/test/java/management/DirectorTest.java
@@ -0,0 +1,51 @@
+package management;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class DirectorTest {
+    Director director;
+
+    @Before
+    public void before(){
+        director = new Director("John", "ABCD10", 100.00, "sales", 5000.0);
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("John", director.getName());
+    }
+
+    @Test
+    public void hasSalary() {
+        assertEquals(100.00, director.getSalary(), 0.0);
+    }
+
+    @Test
+    public void hasNI() {
+        assertEquals("ABCD10", director.getNI());
+    }
+
+    @Test
+    public void hasDeptName(){
+        assertEquals("sales", director.getDeptName());
+    }
+
+    @Test
+    public void canRaiseSalary() {
+        director.raiseSalary(50.00);
+        assertEquals(150.00, director.getSalary(), 0.0);
+    }
+
+    @Test
+    public void canPayBonus() {
+        assertEquals(1, director.payBonus(), 0.0);
+    }
+
+    @Test
+    public void hasBudget() {
+        assertEquals(5000.0, director.getBudget(), 0.0);
+    }
+}
--- src/test/java/techStaff/DatabaseAdminTest.java
+++ src/test/java/techStaff/DatabaseAdminTest.java
@@ -1,4 +1,44 @@
 package techStaff;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class DatabaseAdminTest {
+
+    DatabaseAdmin dbAdmin;
+
+    @Before
+    public void before() {
+        dbAdmin = new DatabaseAdmin("Jane", "QPE12", 500.00);
+
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("Jane", dbAdmin.getName());
+    }
+
+    @Test
+    public void hasNI() {
+        assertEquals("QPE12", dbAdmin.getNI());
+    }
+
+    @Test
+    public void hasSalary() {
+        assertEquals(500.0, dbAdmin.getSalary(), 0.0);
+    }
+
+    @Test
+    public void canRaiseSalary() {
+        dbAdmin.raiseSalary(150.0);
+        assertEquals(650.0, dbAdmin.getSalary(), 0.0);
+    }
+
+    @Test
+    public void canPayBonus() {
+        assertEquals(5, dbAdmin.payBonus(), 0.0);
+
+    }
 }
