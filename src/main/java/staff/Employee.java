765ac28b25c3b02b49a6657120db71fcbbf8ef3a
Clau <clau.rjimenez@gmail.com>
2022-02-09 15:40:29 +0000

    small fixes

--- src/main/java/staff/Employee.java
+++ src/main/java/staff/Employee.java
@@ -15,7 +15,7 @@ public abstract class Employee {
         return name;
     }
 
-    public void setName(String name) {
+    public final void setName(String name) {
         if (name != null) {
             this.name = name;
         }
